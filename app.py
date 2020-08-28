4bd7eec4f73638edd3d2dcb44afefa373ebf5b1d
Kurt Pessa <kpessa@gmail.com>
2020-08-27 23:55:03 -0400

    working on flask - frozen app to deploy to github pages

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import *
+app = Flask(__name__)
+
+@app.route('/index.html')
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/data/beneficiary.html')
+def beneficiary():
+    return render_template("beneficiary.html")
+
+@app.route('/data/inpatient.html')
+def inpatient():
+    return render_template("inpatient.html")
+
+@app.route('/data/outpatient.html')
+def outpatient():
+    return render_template("outpatient.html")
+
+@app.route('/data/potential_fraud.html')
+def potential_fraud():
+    return render_template("potential_fraud.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
